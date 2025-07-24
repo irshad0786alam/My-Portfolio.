7add7fa0f04225b2fccab3a9ca6ad9a929cf0842
Irshad Alam <irshadalam3092006@gmail.com>
2025-07-24 11:56:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,13 +26,13 @@ $(document).ready(function(){
 
 // typing animation script
 var typed = new Typed(".typing",{
-    strings:["Web Developer","C++ Developer","Data Analyst"],
+    strings:["Software Developer","Java Developer","Data Analyst"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
 });
 var typed = new Typed(".typing-2",{
-    strings:["Web Developer","C++ Developer","Data Analyst"],
+    strings:["Sotware Developer","Java Developer","Data Analyst"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
